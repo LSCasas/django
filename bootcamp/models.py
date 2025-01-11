66723f527e8f24f829f7b5b05866709f511662f9
Luis Casas <luisscasas45@gmail.com>
2025-01-11 16:03:37 -0600

    FEAT/ Add queries for koders by mentor and exclude last bootcamp

--- bootcamp/models.py
+++ bootcamp/models.py
@@ -30,16 +30,13 @@ class Koder(models.Model):
         ("s", "Small"), ("m", "Medium"),
         ("l", "Large")
     ]
-    first_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=254)
     last_name = models.CharField(max_length=255)
-    generation = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=25)
     status = models.CharField(max_length=8, choices=STATUSES, default="active")
-    birthday = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(blank=True, null=True)
-
+    
     generation = models.ForeignKey(Generation, models.PROTECT, related_name="koders")
     
     def __str__(self):
