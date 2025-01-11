f2653d94097743bd7b90e50ea633c981be68d640
Luis Casas <luisscasas45@gmail.com>
2025-01-11 12:15:03 -0600

    FEAT/ Add models for Bootcamp, Generation, and Mentor

--- bootcamp/models.py
+++ bootcamp/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Bootcamp(models.Model):
+    """Bootcamp Model."""
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.name} "
+
+class Generation(models.Model):
+    """Generation Model."""
+    number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    
+    bootcamp = models.ForeignKey(Bootcamp, models.PROTECT, related_name="generations")
+    
+    def __str__(self):
+        return f"{self.number} {self.bootcamp.name}"
+
 
 class Koder(models.Model):
     """Koder Model."""
@@ -23,5 +40,24 @@ class Koder(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(blank=True, null=True)
 
+    generation = models.ForeignKey(Generation, models.PROTECT, related_name="koders")
+    
     def __str__(self):
         return f"FirstName -> {self.first_name}, LastName -> {self.last_name}"
+    
+
+class Mentor(models.Model):
+    """Mentor Model."""
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=25)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    generations = models.ManyToManyField(Generation, related_name="mentors")
+
+    def __str__(self):
+        return f"id -> {self.pk}  {self.first_name} {self.last_name}"
+
+
--- bootcamp/views.py
+++ bootcamp/views.py
@@ -2,27 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
-
+from bootcamp.models import Koder
 def list_koder(request):
-    context = {
-        "bootcamp": {"name": "Python", "module": "Django"},
-        "koders": [
-        {"name": "Benjamin", "generation": "1g", "bootcamp": "Python", "is_active": True},
-        {"name": "Luis", "generation": "1g", "bootcamp": "Python", "is_active": True},
-        {"name": "Irving", "generation": "1g", "bootcamp": "Python", "is_active": False}
-    ]
-    }
+    koders = Koder.objects.all()
+    return HttpResponse(koders)
 
-    template = loader.get_template("templates/list_koders.html")
+    # context = {
+    #     "bootcamp": {"name": "Python", "module": "Django"},
+    #     "koders": [
+    #     {"name": "Benjamin", "generation": "1g", "bootcamp": "Python", "is_active": True},
+    #     {"name": "Luis", "generation": "1g", "bootcamp": "Python", "is_active": True},
+    #     {"name": "Irving", "generation": "1g", "bootcamp": "Python", "is_active": False}
+    # ]
+    # }
+    # template = loader.get_template("templates/list_koders.html")
 
 
-    return HttpResponse(template.render(context, request))
-
-def get_koder(request, id):
-    nombres = ["Juan", "Ana", "Luis", "Maria", "Carlos"]
-    
-    if 1 <= id <= len(nombres):  
-        nombre = nombres[id - 1]  
-        return HttpResponse(f"El nombre con ID {id} es: {nombre}")
-    else:
-        return HttpResponse(f"ID {id} no es válido. Por favor, ingrese un ID entre 1 y {len(nombres)}.")
+def get_koder(request, id):  
+    koder = Koder.objects.get(id=id)
+    return HttpResponse(f"Koder: FirstName -> {koder.first_name}, LastName -> {koder.last_name}")
